411251b19c443729ddaab80e9456d454759e5021
devano2068 <devano27921@gmail.com>
2026-02-23 17:58:26 +0700

    first commit

---
+++ namaproject.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//variabel global
+int sks;
+
+int main(){
+    //variabel lokal
+    int umur;
+    umur = 20;
+
+    cout << "Halo Mas Abra" << endl;
+    cout << 10 << endl;
+    cout << umur;
+}
+
